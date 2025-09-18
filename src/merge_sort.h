5caed6645201e8b1c43fa286a55705a452ca1133
Skandalf <gioelescandaletti@gmail.com>
2025-09-18 20:21:03 +0200

    Fix name

--- src/merge_sort.h
+++ src/merge_sort.h
@@ -1,19 +1,21 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
+#include <algorithm>
 #include <omp.h>
+#include <random>
 #include <stdio.h>
 #include <stdlib.h>
-#include <algorithm>
-#include <random>
 
 #include <vector>
 
-void printArray(const std::vector<int>& arr);
-void merge(std::vector<int>& arr, int left, int mid, int right);
-void parallel_merge_sort(std::vector<int>& arr, int left, int right, int depth = 0);
-void read_input(std::vector<int>& arr, int n, char* inputs[]);
-void init_default_vector(std::vector<int>& arr);
-std::pair<int, int> selection_problem(const std::vector<int>& A, const std::vector<int>& B, int k);
+void printArray(const std::vector<int> &arr);
+void merge(std::vector<int> &arr, int left, int mid, int right);
+void parallel_merge_sort(std::vector<int> &arr, int left, int right,
+                         int depth = 0);
+void read_input(std::vector<int> &arr, int n, char *inputs[]);
+void init_default_vector(std::vector<int> &arr);
+std::pair<int, int> selection(const std::vector<int> &A,
+                              const std::vector<int> &B, int k);
 
 #endif
